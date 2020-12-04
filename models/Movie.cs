5efe091bfc07446e004968fe4dee5dfafbf79f59
Matthew-swin <103219809@swin.edu.au>
2020-12-04 13:50:24 +1100

    moethods did

--- models/Movie.cs
+++ models/Movie.cs
@@ -1,6 +1,6 @@
 using System;
 using System.Collections.Generic;
-// using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Data.SqlClient;
 
 namespace models
@@ -11,45 +11,83 @@ namespace models
         public string title { get; set; }
         public Int16 relYear { get; set; }
         public Int16 runTime { get; set; }
+        //
+        public int methodBitch;
+        // SQL interfaces and objects
+        IConfiguration configuration;
+        SqlConnectionStringBuilder stringBuilder = new SqlConnectionStringBuilder();
+        string connectionString;
         
-        
-        //use dis in controller
-        public DateTime currentDateTime = DateTime.Now;
+        public Movie(){}
+        public Movie(IConfiguration iconfig)
+        {
+            
+            
+            this.configuration = iconfig;
+            try
+            {
+                this.stringBuilder.DataSource = "no.database.here.com";
+                this.stringBuilder.InitialCatalog = "ls";
+                this.stringBuilder.UserID = "Wally";
+                this.stringBuilder.Password = "Where";
+                this.connectionString = this.stringBuilder.ConnectionString;
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
 
-        // SqlConnectionStringBuilder stringBuilder = new SqlConnectionStringBuilder();
-        // IConfiguration configuration;
-        // string connectionString;
+                this.stringBuilder.DataSource = this.configuration.GetSection("DbConnectionStrings").GetSection("Url").Value;
+                this.stringBuilder.InitialCatalog = this.configuration.GetSection("DbConnectionStrings").GetSection("Database").Value;
+                this.stringBuilder.UserID = this.configuration.GetSection("DbConnectionStrings").GetSection("User").Value;
+                this.stringBuilder.Password = this.configuration.GetSection("DbConnectionStrings").GetSection("Password").Value;
+                this.connectionString = this.stringBuilder.ConnectionString;
+            }
 
-        //     public int NumActors(string movieName){
-        //     List<Movie> Movies = new List<Movie>();
-        //     SqlConnection conn = new SqlConnection(connectionString);
-        //     string queryString = $"Select * From Movie";
-        //     SqlCommand cmd = new SqlCommand(queryString, conn);
-        //     conn.Open();
-        //     string result = "";
-        //     using (SqlDataReader reader = cmd.ExecuteReader())
-        //     {
-        //         while (reader.Read())
-        //         {
-        //             result += reader[0] + " | " + reader[1] + reader[2] + reader[3] + "\n";
+        }
+        //end of SQL stuff
 
-        //             Movies.Add(
-        //                 new Movie()
-        //                 {
-        //                     movieNo = (int)reader[0],
-        //                     title = reader[1].ToString(),
-        //                     relYear = (Int16)reader[2],
-        //                     runTime = (Int16)reader[3]
-        //                 });
-        //                 reader.Close();
-        //         }
-        //     }
-        //     int total = 0;
-        //     foreach (var movie in Movies)
-        //     {   
-        //         total = total + (int)movie.runTime;
-        //     }
-        //     return total;
-        // }
+        //methods to test
+        public int NumActor()
+        {
+            SqlConnection conn = new SqlConnection(connectionString);
+            string queryString = $"Select count(ACTORNO) FROM CASTING C LEFT JOIN MOVIE M ON M.MOVIENO = C.MOVIENO WHERE M.TITLE = {this.title}";
+            SqlCommand cmd = new SqlCommand(queryString, conn);
+            conn.Open();
+
+            using (SqlDataReader reader = cmd.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    this.methodBitch = (int)reader[0];         
+                }
+            }
+
+            return methodBitch;
+        }
+
+        public int GetAge()
+        {
+            DateTime currentDateTime = DateTime.Now;
+            currentDateTime.ToString("yyyy");
+            int currdate = Convert.ToInt32(currentDateTime);
+
+            SqlConnection conn = new SqlConnection(connectionString);
+            string queryString = $"Select Relyear From Movie where Title = {this.title}";
+            SqlCommand cmd = new SqlCommand(queryString, conn);
+            conn.Open();
+
+            using (SqlDataReader reader = cmd.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    this.relYear = (Int16)reader[0];
+                }
+            }
+            int poop;
+            return poop = currdate - (int)this.relYear;
+        }
     }
 }
--- test/UnitTest1.cs
+++ test/UnitTest1.cs
@@ -1,10 +1,12 @@
 using System;
 using Xunit;
+using models;
 
 namespace test
 {
     public class UnitTest1
     {
+        
         [Fact]
         public void Test1()
         {
